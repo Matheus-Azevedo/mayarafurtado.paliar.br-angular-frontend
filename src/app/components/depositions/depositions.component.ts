56e0fd31856280ae2241196eaeccbbfa07e0c3ff
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-07-21 09:03:01 -0300

    created components for the lading page

---
+++ src/app/components/depositions/depositions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-depositions',
+  standalone: true,
+  imports: [],
+  templateUrl: './depositions.component.html',
+  styleUrl: './depositions.component.scss'
+})
+export class DepositionsComponent {
+
+}
